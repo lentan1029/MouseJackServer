b4422510a8a0a04a07d07bd98970b5f0dcd14429
lentan <lentan1029@gmail.com>
2016-08-29 14:34:38 -0700

    initial commit for github repo

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
 var port = 3000;
+var x = 10;
+var y = 10;
 
 app.get('/', function(req, res) {
-  res.status(200).send('test');
-})
+  res.status(200).json({
+    x: x,
+    y: y
+  });
+});
+
+app.post('/', function(req, res) {
+  
+});
 
 app.listen(port, function() {
   console.log('app listening on port', port);
