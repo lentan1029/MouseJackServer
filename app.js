389dfceca1911add232b3842788099c9c05ce8db
lentan <lentan1029@gmail.com>
2016-08-29 14:40:10 -0700

    Add body-parser and handle POST requests

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var port = 3000;
-var x = 10;
-var y = 10;
+var x = 0;
+var y = 0;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.status(200).json({
@@ -13,7 +15,8 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res) {
-
+  console.log(req.body);
+  res.send('sent data');
 });
 
 app.listen(port, function() {
