4653c0b672e3e534456e59d270e7f283dd75d724
lentan <lentan1029@gmail.com>
2016-08-30 09:43:15 -0700

    modified renderd3

--- public/renderd3.js
+++ public/renderd3.js
@@ -1,1 +1,10 @@
-alert('hello');
+var svg = d3.select('#nav')
+   .append('div')
+   .classed('svg-container', true) //container class to make it responsive
+   .append('svg')
+   //responsive SVG needs these 2 attributes and no width and height attr
+   .attr('preserveAspectRatio', 'xMinYMin meet')
+   .attr('viewBox', '0 0 600 400')
+   .attr('fill', 'blue')
+   //class to make it responsive
+   .classed('svg-content-responsive', true); 
